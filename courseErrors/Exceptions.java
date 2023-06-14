4ee0d75678ccf6e33053cc8b2d6f8b13feb408c0
PoeticCrane <poeticcrane@gmail.com>
2023-06-14 06:45:41 +0300

    first hwork

---
+++ courseErrors/Exceptions.java
@@ -0,0 +1,18 @@
+package courseErrors;
+
+public class Exceptions {
+    public void throwNullPointerException() {
+        String str = null;
+        str.length();
+    }
+
+    public void throwArrayIndexOutOfBoundsException() {
+        int[] array = {1, 2, 3};
+        int element = array[3];
+    }
+
+    public void throwNumberFormatException() {
+        String str = "abc";
+        int number = Integer.parseInt(str);
+    }
+}
---
+++ courseErrors/hw1.java
@@ -0,0 +1,28 @@
+package courseErrors;
+
+/**
+ * hw1
+ */
+public class hw1 {
+
+    public static void main(String[] args) {
+          Exceptions exceptions = new Exceptions();
+        try {
+            exceptions.throwNullPointerException();
+        } catch (NullPointerException e) {
+            System.out.println("Перехвачено исключение: " + e);
+        }
+
+        try {
+            exceptions.throwArrayIndexOutOfBoundsException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Перехвачено исключение: " + e);
+        }
+
+        try {
+            exceptions.throwNumberFormatException();
+        } catch (NumberFormatException e) {
+            System.out.println("Перехвачено исключение: " + e);
+        }
+    }
+}
